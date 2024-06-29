7e9de7f9e0eeaf96351164d0416329162cb81cc8
maowarima5647 <mawarif217@gmail.com>
2024-06-29 04:45:40 -0700

    Add files via upload

---
+++ Ralphs/index.js
@@ -0,0 +1,11 @@
+/*var tabButton=document.querySelectorAll(".tab-container.button-container button")
+var tabpanel = document.querySelectorAll(".tab-container.tabPanel")
+
+function(){
+    tabButton.forEach (function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+
+    )
+    }*/
+    
